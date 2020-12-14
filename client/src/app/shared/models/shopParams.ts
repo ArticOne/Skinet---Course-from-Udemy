8d70c42f2c49ea5d3b4fa811026314d80d513b4c
Stefan Mitrović <stefmit10@gmail.com>
2020-12-14 20:11:29 +0100

    End of Section 9

---
+++ client/src/app/shared/models/shopParams.ts
@@ -0,0 +1,8 @@
+export class ShopParams {
+  brandId = 0; // selected brandID
+  typeId = 0; // selected typeID
+  sort = 'name'; // selected sort
+  pageNumber = 1;
+  pageSize = 6;
+  search: string;
+}
