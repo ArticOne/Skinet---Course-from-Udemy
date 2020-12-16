2eacd5d651355b044e15346c4e2481837d82ef6b
Stefan Mitrović <stefmit10@gmail.com>
2020-12-16 19:17:46 +0100

    End of Section 11

--- client/src/app/shop/shop-routing.module.ts
+++ client/src/app/shop/shop-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ShopComponent } from './shop.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 
+
 const routes: Routes = [
   { path: '', component: ShopComponent },
   { path: '/:id', component: ProductDetailsComponent },
